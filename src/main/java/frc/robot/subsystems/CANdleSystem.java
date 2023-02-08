7f6990a58f2d5eb9f88cfeaab21002b5aaab83b2
Nick Avtges <navtges@gmail.com>
2023-02-07 20:48:38 -0500

    clear animations before switching to rainbow

--- src/main/java/frc/robot/subsystems/CANdleSystem.java
+++ src/main/java/frc/robot/subsystems/CANdleSystem.java
@@ -45,6 +45,7 @@ public class CANdleSystem extends SubsystemBase {
         configAll.brightnessScalar = 0.1;
         configAll.vBatOutputMode = VBatOutputMode.Modulated;
         m_candle.configAllSettings(configAll, 100);
+        clearAllAnims();
         rainbow();
     }
 
