6e1c2ae83bc1debb55711694205afc59f98bbdf0
Nick Avtges <navtges@gmail.com>
2023-02-09 20:53:00 -0500

    boot sequence

--- src/main/java/frc/robot/subsystems/CANdleSystem.java
+++ src/main/java/frc/robot/subsystems/CANdleSystem.java
@@ -13,9 +13,12 @@ import com.ctre.phoenix.led.*;
 import com.ctre.phoenix.led.CANdle.LEDStripType;
 import com.ctre.phoenix.led.CANdle.VBatOutputMode;
 import com.ctre.phoenix.led.ColorFlowAnimation.Direction;
+import com.ctre.phoenix.led.LarsonAnimation.BounceMode;
 
 public class CANdleSystem extends SubsystemBase {
   private final int LEDS_PER_ANIMATION = 128;
+  private int m_state = 0;
+  private int counter = 0;
     private final CANdle m_candle = new CANdle(Constants.CANDLE_ID);
     private boolean m_clearAllAnims = false;
     private boolean m_animDirection = false;
@@ -66,12 +69,37 @@ public class CANdleSystem extends SubsystemBase {
         m_toAnimate = new ColorFlowAnimation(100, 0, 120, 0, 0.2, LEDS_PER_ANIMATION, Direction.Forward);
     }
 
+    public void larsonColor(int r, int g, int b) {
+        m_toAnimate = new LarsonAnimation(r, g, b, 0, 0.2, LEDS_PER_ANIMATION, BounceMode.Front, 5, 0);
+    }
+
     public void rainbow() {
         m_toAnimate = new RainbowAnimation(1, 0.7, LEDS_PER_ANIMATION, m_animDirection, 0);
     }
 
     @Override
     public void periodic() {
+        switch(m_state){
+            case 0: larsonColor(255,0,0);
+            if (counter++>100) {
+                m_state ++;
+                counter = 0;
+            }
+            break;
+            case 1: larsonColor(0,255,0);
+            if (counter++>100) {
+                m_state ++;
+                counter = 0;
+            }
+            break;
+            case 2: larsonColor(0,0,255);
+            if (counter++>100) {
+                m_state ++;
+                counter = 0;
+            }
+            break;
+            default: rainbow();
+        }
         // This method will be called once per scheduler run
         if(m_toAnimate == null) {
             if(!m_setAnim) {
