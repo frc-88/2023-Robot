544d1d9f49ffd40ab505336678a4f0c7db07d72c
pixelated-being <ajd0929@gmail.com>
2023-02-19 10:23:57 -0500

    ros integration woooooo

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -103,4 +103,13 @@ public final class Constants {
 	
     // Limelight
     public static final String LIMELIGHT_NAME = "limelight";
+
+
+    // Coprocessor
+    public static final String COPROCESSOR_ADDRESS = "10.0.88.44";
+    public static final int COPROCESSOR_PORT = 5800;
+    public static final double COPROCESSOR_UPDATE_DELAY = 1/30;
+    public static final double COPROCESSOR_UPDATE_DELAY_OFFSET = 0.01;
+    public static final double COPROCESSOR_SLOW_UPDATE_DELAY = 1/5;
+    public static final double COPROCESSOR_SLOW_UPDATE_DELAY_OFFSET = 0.02;
 }
