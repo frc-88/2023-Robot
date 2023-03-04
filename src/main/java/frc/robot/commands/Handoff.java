eae731a7c020ba748b72913544f70bf30e3c5301
Paul Terrasi <paul.a.terrasi@gmail.com>
2023-03-04 13:34:59 -0500

    Length short handoff

--- src/main/java/frc/robot/commands/Handoff.java
+++ src/main/java/frc/robot/commands/Handoff.java
@@ -20,7 +20,7 @@ public class Handoff extends SequentialCommandGroup {
     addCommands(
       arm.sendArmToStateAndEnd(coneMode ? ArmStates.getConeFromIntake1 : ArmStates.getCubeFromIntake1).deadlineWith(grab.get(), intake.stowFactory()),
       arm.sendArmToStateAndEnd(coneMode ? ArmStates.getConeFromIntake2 : ArmStates.getCubeFromIntake2).deadlineWith(grab.get(), intake.handoffFactory()).withTimeout(coneMode ? 0.3 : 1),
-      arm.sendArmToState(ArmStates.stow).alongWith(grab.get(), intake.handoffFactory()).until(() -> arm.isAtTarget(ArmStates.stow, runShort ? 45 : 2))
+      arm.sendArmToState(ArmStates.stow).alongWith(grab.get(), intake.handoffFactory()).until(() -> arm.isAtTarget(ArmStates.stow, runShort ? 30 : 2))
     );
   }
 }
