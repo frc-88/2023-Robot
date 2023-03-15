26eabc6984b6a7930601e42cc56a1f8695d324ae
Paul Terrasi <paul.a.terrasi@gmail.com>
2023-03-15 17:54:11 -0400

    Multi-convert-alliance

--- convert_alliance.py
+++ convert_alliance.py
@@ -1,25 +1,29 @@
 import json
-import os
+import glob
+
 
 JSON_PATH = "src/main/deploy/pathplanner/"
-FILE_TO_CONVERT = "CenterEngage.path"
 CONVERT_FROM_RED = True
 FIELD_WIDTH = 16.54
 
+files_to_convert = ["CenterEngage.path"]
+#Uncomment to do all files
+files_to_convert = glob.glob(JSON_PATH + ("Red" if CONVERT_FROM_RED else "Blue") + "*.path")
 
-with open(JSON_PATH + ("Red" if CONVERT_FROM_RED else "Blue") + FILE_TO_CONVERT) as f:
-    data = json.load(f)
-    
-for waypoint in data["waypoints"]:
-    if waypoint["anchorPoint"] is not None:
-        waypoint["anchorPoint"]["x"] = FIELD_WIDTH - waypoint["anchorPoint"]["x"]
-    if waypoint["nextControl"] is not None:
-        waypoint["nextControl"]["x"] = FIELD_WIDTH - waypoint["nextControl"]["x"]
-    if waypoint["prevControl"] is not None:
-        waypoint["prevControl"]["x"] = FIELD_WIDTH - waypoint["prevControl"]["x"]
+for file_to_convert in files_to_convert:
+    with open(file_to_convert) as f:
+        data = json.load(f)
         
-with open(JSON_PATH + ("Blue" if CONVERT_FROM_RED else "Red") + FILE_TO_CONVERT, 'w') as f:
-    json.dump(data, f, indent=4)
+    for waypoint in data["waypoints"]:
+        if waypoint["anchorPoint"] is not None:
+            waypoint["anchorPoint"]["x"] = FIELD_WIDTH - waypoint["anchorPoint"]["x"]
+        if waypoint["nextControl"] is not None:
+            waypoint["nextControl"]["x"] = FIELD_WIDTH - waypoint["nextControl"]["x"]
+        if waypoint["prevControl"] is not None:
+            waypoint["prevControl"]["x"] = FIELD_WIDTH - waypoint["prevControl"]["x"]
+            
+    with open(JSON_PATH + ("Blue" if CONVERT_FROM_RED else "Red") + file_to_convert, 'w') as f:
+        json.dump(data, f, indent=4)
         
     
 
